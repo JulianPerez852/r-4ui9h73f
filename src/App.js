26a98bcc94d6791406f7edf6accb92031e2b3566
JulianPerez852 <jdph852@gmail.com>
2019-06-12 20:05:27 -0500

    Hago commmit para reto de react

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      text: '',
+    }
+    this.copiador=this.copiador.bind(this);
+  }
+
+  copiador(e){
+    this.setState({
+      text: e.target.value
+    })
+  }
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" placeholder="Empieza a escribir algo" onChange={this.copiador} />
+        <p className="repeater">{this.state.text}</p>
       </div>
     );
   }
